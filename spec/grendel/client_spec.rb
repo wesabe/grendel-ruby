4993e7110632019294e74c097082a57bbb48cb47
Brian Donovan <brian.donovan@gmail.com>
2010-04-27 18:43:17 -0700

    More idiomatic rspec.

--- spec/grendel/client_spec.rb
+++ spec/grendel/client_spec.rb
@@ -4,16 +4,16 @@ describe "Grendel::Client" do
   before do
     @client = Grendel::Client.new("http://example.com")
   end
-  
+
   describe "new method" do
     it "should set the base_uri" do
       @client.base_uri.should == "http://example.com"
     end
   end
-  
+
   describe "users method" do
     it "should return a Grendel::UserManager" do
-      @client.users.class.should be(Grendel::UserManager)
+      @client.users.should be_a(Grendel::UserManager)
     end
   end
 end
